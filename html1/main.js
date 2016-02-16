8b8ed6411b4cd75cd37799c38c544837294da2b7
coppieeee <coppieee@gmail.com>
2016-02-16 21:29:27 +0900

    サンプル追加

--- html1/main.js
+++ html1/main.js
@@ -9,17 +9,33 @@ jQuery(function($){
 	var canvas = $('#canvas')[0];
 	//var canvas = document.getElementById('canvas');
 	var ctx = canvas.getContext('2d');
-	//円を書く
+	// //円を書く
 	// ctx.beginPath();
 	// var x = 70;
 	// var y = 250;
-	// ctx.arc(x,250,20,0,Math.PI*2,false);
-	// ctx.stroke();
-	// var f = function(){
-	// 	四角
-	// 	ctx.clearRect(0,0,500,500);
-	var x = 50;
+	// // ctx.arc(x,250,20,0,Math.PI*2,false);
+	// // ctx.stroke();
 
+	// // 円を10回書く
+	// for(var i=0;i<10;i++){
+	// 	x = x + 10;
+	// 	ctx.beginPath();
+	// 	ctx.arc(x,250,20,0,Math.PI*2,false);
+	// 	ctx.stroke();
+	// }
+
+	// //四角
+	// ctx.clearRect(0,0,500,500);
+	
+
+	// // 1000ミリ秒ごとに'hello'を表示
+	// var g = function(){
+	// 	console.log('hello');
+	// 	setTimeout(g,1000);
+	// };
+	// g();
+
+	var x = 50;
 	var f = function(){
 		ctx.clearRect(0,0,500,500);
 		x = x + 1;
@@ -29,19 +45,13 @@ jQuery(function($){
 		setTimeout(f,17);
 	};
 	f();
-
+	
 	var sum = function(x,y){
 		return x + y;
 	};
-	var ans = sum(1,3);
-	var ans2 = sum('HEllo','world');
-	console.log(ans2);
-
-	// var g = function(){
-	// 	console.log('hello');
-	// 	setTimeout(g,1000);
-	// };
-	// g();
+	// var ans = sum(1,3);
+	// var ans2 = sum('HEllo','world');
+	// console.log(ans2);
 
 	// 	x = x + 1;
 	// 	// y = y - 1;
